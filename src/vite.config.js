e1e938f4f7ad71eb28d97dfa5459432c99f6d10a
austinseger <austin.seger.1024@gmail.com>
2024-09-02 14:17:12 -0700

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure this is correctly set to the build output directory
+  },
   server: {
     port: 3000,
     open: true,
